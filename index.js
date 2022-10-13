9e0763196574f54139c5f839d533c0e5f881d8aa
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-10-13 15:18:28 +0700

    Write middleware to log url and datetime

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
 const members = require('./Members');
 
 const app = express();
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format(
+      'MMMM Do YYYY, h:mm:ss a'
+    )}`
+  );
+  next();
+};
+
+// Init middleware
+app.use(logger);
 
 // Get All Members
 app.get('/api/members', (req, res) => res.json(members));
